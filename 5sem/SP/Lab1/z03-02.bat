7776edc0959ec7aa602978f95cd509da0627da43
DimaDD <dimatruba2004@yandex.ru>
2023-09-12 09:54:00 +0300

    Lab1 OS+SP rework, lab2, 3 TPO

--- 5sem/SP/Lab1/z03-02.bat
+++ 5sem/SP/Lab1/z03-02.bat
@@ -1,9 +1,12 @@
 @echo off
 title z03-02
 chcp 65001 > nul 2>&1
+@for /f "tokens=*" %%A in ('wmic datafile where "name='%~dpnx0'" get creationdate ^| findstr /r "[0-9]"') do @set creationdate=%%A
+@for /f "tokens=*" %%B in ("%creationdate%") do @set creationdate=%%B
+@for /f "delims=" %%C in ('powershell "(Get-Item '%~dpnx0').CreationTime.ToString('yyyy-MM-dd HH:mm:ss')"') do @set formatteddate=%%C
 cls
 echo --Имя этого bat-файла:     %~n0
-echo --Этот файл создан:	%~t0
+echo --Этот файл создан:	%formatteddate%
 echo --Путь bat-файла:     %~dpnx0 
 :: or type %~f0 for full file name
 pause
