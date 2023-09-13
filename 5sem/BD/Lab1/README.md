27a8880b57ad2828aa2892200144dfcb0ad1e3b5
DimaDD <dimatruba2004@yandex.ru>
2023-09-14 00:48:01 +0300

    Lab1 BD

---
+++ 5sem/BD/Lab1/README.md
@@ -0,0 +1,3 @@
+# Не знаю зачем, но может понадобится:
+## Проблема с database not open
+	https://ermannkara.wordpress.com/2017/08/10/ora-01589-must-use-resetlogs-or-noresetlogs-option-for-database-open/
--- 5sem/PSKP/Lab1/01-01.js
+++ 5sem/PSKP/Lab1/01-01.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http.createServer(function(request, response){
      
-    response.end("Hello World");
+    response.end("<h1>Hello world!</h1>");
      
 }).listen(3000, "127.0.0.1", function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
