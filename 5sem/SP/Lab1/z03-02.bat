0cad0421aed6bf6d560d28ad3742e42eca66aabc
DimaDD <dimatruba2004@yandex.ru>
2023-09-18 20:43:29 +0300

    Lab1 OS&SP: обновил 2 батник до идеала

--- 5sem/SP/Lab1/z03-02.bat
+++ 5sem/SP/Lab1/z03-02.bat
@@ -1,12 +1,14 @@
 @echo off
 title z03-02
 chcp 65001 > nul 2>&1
-@for /f "tokens=*" %%A in ('wmic datafile where "name='%~dpnx0'" get creationdate ^| findstr /r "[0-9]"') do @set creationdate=%%A
-@for /f "tokens=*" %%B in ("%creationdate%") do @set creationdate=%%B
-@for /f "delims=" %%C in ('powershell "(Get-Item '%~dpnx0').CreationTime.ToString('yyyy-MM-dd HH:mm:ss')"') do @set formatteddate=%%C
-cls
-echo --Имя этого bat-файла:     %~n0
-echo --Этот файл создан:	%formatteddate%
-echo --Путь bat-файла:     %~dpnx0 
+set "BatchName=%~n0"
+for /f "tokens=1,2" %%i in ('dir "%BatchName%.bat" /tc ^| findstr /i "%BatchName%.bat"') do (
+    set "CreationDate=%%i"
+    set "CreationTime=%%j"
+)
+
+echo -- имя этого bat-файла: %~n0
+echo -- этот bat-файл создан: %CreationDate% %CreationTime%
+echo -- путь bat-файла: %~dp0
 :: or type %~f0 for full file name
 pause
