d52cc76962253e9f4286296a81a57bfb99f133df
DimaDD <dimatruba2004@yandex.ru>
2023-10-04 19:52:48 +0300

    Full Lab3 BD

--- 5sem/BD/Lab3/Lab3.sql
+++ 5sem/BD/Lab3/Lab3.sql
@@ -8,6 +8,7 @@ select * from V$INSTANCE;
 select * from v$option;
 
 --4
+-- create TDS_PDB with admin TDS_PDB_ADMIN in DB Configuration Assistant
 -- Как создать? (Актуально для Oracle 19c)
 -- 1. Запускаем DB Configuration Assistant (находим в поиске Windows или
 -- CMD -> dbca
@@ -24,7 +25,8 @@ select * from dba_pdbs;
 
 --6
 alter session set container = TDS_PDB;
-alter session set "_ORACLE_SCRIPT" = false;
+alter pluggable database TDS_PDB open;
+alter session set "_ORACLE_SCRIPT" = true;
 --alter session set container = CDB$ROOT; вернутся к истокам
 
 -- Смотрим к чему мы подключены
@@ -66,7 +68,7 @@ select * from dba_roles where ROLE like '%RL%';
 drop role TDS_PDB_RL;
 
 -- profile
--- Если не создается профиль в PDB, то просто обновляйте ваш коннект, 
+-- Если не создается профиль в PDB, то просто обновляйся ваш коннект, 
 -- мне помогло)))
 create profile TDS_PDB_PROFILE limit
   password_life_time 365
@@ -78,7 +80,6 @@ create profile TDS_PDB_PROFILE limit
   
 select * from dba_profiles where PROFILE like '%TDS_PDB_PROFILE%';
 drop profile TDS_PDB_PROFILE;
-  
   
 -- user
 create user U1_TDS_PDB 
@@ -110,8 +111,10 @@ drop user U1_TDS_PDB;
 --      В моём случае TDS_PDB.mshome.net, все зависит от имени PDB, которую вы
 --      создавали и от global_name, вывод которой можно чекнуть внизу скриптом
 --      У меня это ORCL.mshome.net, нужна эта часть: mshome.net
+--      Думаю принцип понятен)
+
 select global_name from global_name;
-s
+
 create table TDS_PDB_TABLE 
 (
     id int primary key,
@@ -149,18 +152,56 @@ grant
     SYSDBA
 to C##TDS container = all;
 
--- Если можно создать две таблицы, значит они создаются в разных DB
+select * from dba_users where USERNAME like '%C##%';
 
--- Запускать в ConnectToCDB_Lab3
+-- !!! Если можно создать две таблицы, значит они создаются в разных DB
+-- Создание коннекта ConnectToCDB_Lab3 как обычный коннект,
+-- просто подключение по логину и паролю, база
+
+-- Создание коннекта ConnectToPDB_Lab3 как обычный коннект,
+-- просто подключение по логину и паролю, база
+
+-- Запускать в коннекте ConnectToCDB_Lab3 по аналогии с заданием 7,
+-- только используем юзера C##TDS
+
 create table x (id int);
 drop table x;
 
--- Запускать в ConnectToPDB_Lab3
+-- Запускать в коннекте ConnectToPDB_Lab3
 create table x (id int);
 drop table x;
 
 -- 10
-select s.sid, s.serial#, s.username, p.pdb_name
-from v$session s
-join v$pdbs p on s.con_id = p.con_id
-where s.type = 'User'
+-- Чтобы не ставить еще одну виртуалку с oracle, проще поставить DataGrip
+-- на свою основную Windows
+-- Инструкция:
+-- 1. Внизу скрипт для создания юзера C##TDS2, через которого мы и будем делать
+-- коннект. Создайте юзера :)
+-- 2. Скачиваем Datagrip по лицензии от jetbrains
+-- 3. После установки слева вверху (окно Database Explorer) нажимаем на "+"
+-- 4. В окне впишите параметры по такому же принципу, как на фото ex10.png 
+-- и нажмите Test Connection
+-- 5. Если ошибки не выдает, значит все хорошо и можно работать уже в DataGrip
+
+
+create user C##TDS2
+    identified by 111;
+    
+grant 
+    connect, 
+    create session, 
+    alter session, 
+    create any table,
+    drop any table,
+    SYSDBA
+to C##TDS2 container = all;
+
+select * from dba_users where USERNAME like '%C##%';
+
+-- 11
+-- В файле Ex11(DataGrip).sql
+
+-- 13
+show pdbs;
+alter pluggable database TDS_PDB open;
+drop pluggable database TDS_PDB including datafiles;
